aa46af0fb5652660bc784253f4a7e9bf93a987a2
804850 <804850@SAIT224140IT.ACDM.DS.SAIT.CA>
2021-02-23 17:50:27 -0700

    Implemented Customer POJOs

--- Capstone/src/java/blb/domain/users/CommericalCustomer.java
+++ Capstone/src/java/blb/domain/users/CommericalCustomer.java
@@ -11,4 +11,86 @@ package blb.domain.users;
  */
 public class CommericalCustomer {
     
+    private String firstname;
+    private String lastname;
+    private String address;
+    private String postalcode;
+    private String email;
+    private String password;
+    private int phonenumber;
+    private char status;
+
+    public CommericalCustomer(String firstname, String lastname, String address, String postalcode, String community, String email, String password, int phonenumber, char status) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.address = address;
+        this.postalcode = postalcode;
+        this.email = email;
+        this.password = password;
+        this.phonenumber = phonenumber;
+        this.status = status;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPostalcode() {
+        return postalcode;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getPhonenumber() {
+        return phonenumber;
+    }
+
+    public char getStatus() {
+        return status;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPostalcode(String postalcode) {
+        this.postalcode = postalcode;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPhonenumber(int phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public void setStatus(char status) {
+        this.status = status;
+    }
+    
+    
+    
 }
